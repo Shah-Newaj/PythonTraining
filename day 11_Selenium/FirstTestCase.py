16a0e66fd7f9a327e544abe1ad6132cd9068d01c
Shah Newaj <newajuiucse@gmail.com>
2022-07-23 16:08:18 +0600

    Selenium Started

---
+++ day 11_Selenium/FirstTestCase.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+serv_obj = Service("C:\Drivers\chromedriver_win32\chromedriver.exe")
+
+driver = webdriver.Chrome(service=serv_obj)
+
+driver.get("https://opensource-demo.orangehrmlive.com/")
+driver.maximize_window()
+
+driver.find_element(By.NAME,"txtUsername").send_keys("Admin")
+driver.find_element(By.ID,"txtPassword").send_keys("admin123")
+driver.find_element(By.ID,"btnLogin").click()
+
+
+
