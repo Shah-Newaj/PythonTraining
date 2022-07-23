01d51e5d5e2d25f60ad89f3e35a2e030dff40440
Shah Newaj <newajuiucse@gmail.com>
2022-07-23 19:26:36 +0600

    Selenium Started Edition Started Added chromedriver in the location of python

--- day 11_Selenium/FirstTestCase.py
+++ day 11_Selenium/FirstTestCase.py
@@ -2,9 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-serv_obj = Service("C:\Drivers\chromedriver_win32\chromedriver.exe")
+# serv_obj = Service("C:\Drivers\chromedriver_win32\chromedriver.exe")
+# driver = webdriver.Chrome(service=serv_obj)
 
-driver = webdriver.Chrome(service=serv_obj)
+driver = webdriver.Chrome()
 
 driver.get("https://opensource-demo.orangehrmlive.com/")
 driver.maximize_window()
