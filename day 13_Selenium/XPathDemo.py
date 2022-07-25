1f915b108b123d153f12ba7d91304e6861824803
Shah Newaj <newajuiucse@gmail.com>
2022-07-25 14:31:12 +0600

    Xpath

--- day 13_Selenium/XPathDemo.py
+++ day 13_Selenium/XPathDemo.py
@@ -15,5 +15,16 @@ driver.maximize_window()
 # driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/div[1]/header[1]/div[3]/div[1]/div[1]/div[2]/form[1]/button[1]").click()
 
 # Relative XPath
-driver.find_element(By.XPATH,"//input[@id='search_query_top']").send_keys("T-shirts")
-driver.find_element(By.XPATH,"//button[@name='submit_search']").click()
+# driver.find_element(By.XPATH,"//input[@id='search_query_top']").send_keys("T-shirts")
+# driver.find_element(By.XPATH,"//button[@name='submit_search']").click()
+
+# or and
+# driver.find_element(By.XPATH,"//input[@id='search_query_top' or @name='search_query']").send_keys("T-shirts")
+# driver.find_element(By.XPATH,"//button[@name='submit_search' and @class='btn btn-default button-search']").click()
+
+# contains() starts-with()
+# driver.find_element(By.XPATH,"//input[contains(@id,'search')]").send_keys("T-shirts")
+# driver.find_element(By.XPATH,"//button[starts-with(@name,'submit')]").click()
+
+#text()
+driver.find_element(By.XPATH,"//a[text()='Women']").click()
