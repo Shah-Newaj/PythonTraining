b06fdfdbede0215d42549c01de00d8753b9dc274
Shah Newaj <newajuiucse@gmail.com>
2022-07-26 19:33:21 +0600

    Different Commands

--- day 15_Selenium/ConditionalCommands.py
+++ day 15_Selenium/ConditionalCommands.py
@@ -10,9 +10,27 @@ driver = webdriver.Chrome()
 driver.get("https://demo.nopcommerce.com/register")
 driver.maximize_window()
 
-#is_displayed()   is_enbaled()
+# is_displayed()   is_enbaled()
 searchbox = driver.find_element(By.XPATH,"//input[@id='small-searchterms']")
 print("Display Status: ",searchbox.is_displayed()) #True
 print("Display Status: ",searchbox.is_enabled()) #True
+
+# is_selected()
+rd_male = driver.find_element(By.XPATH,"//input[@id='gender-male']")
+rd_female = driver.find_element(By.XPATH,"//input[@id='gender-female']")
+
+print("Default radio button status:")
+print("Male Status: ",rd_male.is_selected())
+print("Female Status: ",rd_female.is_selected())
+
+print("After Clicking on Male Radio Button:")
+rd_male.click() #click on male radio button
+print("Male Radio Button Status: ",rd_male.is_selected()) #True
+print("Female Radio Button Status: ",rd_female.is_selected()) #False
+
+print("After Clicking on Female Radio Button:")
+rd_female.click() #click on female radio button
+print("Male Radio Button Status: ",rd_male.is_selected()) #True
+print("Female Radio Button Status: ",rd_female.is_selected()) #False
 
 driver.quit()
