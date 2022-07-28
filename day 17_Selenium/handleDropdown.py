8b0e1985abdb6fa7277eb59341f8d8bef25bdaaf
Shah Newaj <newajuiucse@gmail.com>
2022-07-28 14:25:26 +0600

    Handle Dropdown List

---
+++ day 17_Selenium/handleDropdown.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+serv_obj = Service("C:\Drivers\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(service=serv_obj)
+
+driver.get("https://www.opencart.com/index.php?route=account/register")
+driver.maximize_window()
+
+drpcountry_element = driver.find_element(By.XPATH,"//select[@id='input-country']")
+drpcountry = Select(drpcountry_element)
+
+#select option from the dropdown
+# drpcountry.select_by_visible_text("Bangladesh")
+# drpcountry.select_by_value("10") #Argentina
+# drpcountry.select_by_index(13) #Index
+
+# capture all the options and print them
+alloptions = drpcountry.options
+# print("Total Number of options: ",len(alloptions))
+
+# for opt in alloptions:
+#     print(opt.text)
+
+# Select option from dropdown without using buil-in method
+for opt in alloptions:
+    if opt.text=="Bangladesh":
+        opt.click()
+        break
