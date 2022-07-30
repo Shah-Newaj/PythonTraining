9afc7d113f5b4b77cff731d6331a8be02092e013
Shah Newaj <newajuiucse@gmail.com>
2022-07-30 17:52:05 +0600

    authenticated popup

---
+++ day 18_Selenium/authenticatedpopup.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+serv_obj = Service("C:\Drivers\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(service=serv_obj)
+
+driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth")
+driver.maximize_window()
