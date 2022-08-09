4fa43c40817293f0dcc24d107089080e2f7668c8
Shah Newaj <newajuiucse@gmail.com>
2022-08-09 10:52:58 +0600

    Mouse Hover Right Click Double Click Drag and Drop

---
+++ day 21_Selenium/RightClick.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+serv_obj = Service("C:\Drivers\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(service=serv_obj)
+driver.implicitly_wait(10)
+
+driver.get("https://swisnl.github.io/jQuery-contextMenu/demo.html")
+driver.maximize_window()
+
+button = driver.find_element(By.XPATH,"//span[@class='context-menu-one btn btn-neutral']")
+
+act = ActionChains(driver)
+
+act.context_click(button).perform() #right click
