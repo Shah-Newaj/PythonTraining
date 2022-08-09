a2ec1cec9fa9ee8b0b465566745b58f53a7b8f88
Shah Newaj <newajuiucse@gmail.com>
2022-08-09 19:59:51 +0600

    Right Click

--- day 21_Selenium/RightClick.py
+++ day 21_Selenium/RightClick.py
@@ -16,3 +16,9 @@ button = driver.find_element(By.XPATH,"//span[@class='context-menu-one btn btn-n
 act = ActionChains(driver)
 
 act.context_click(button).perform() #right click
+
+time.sleep(3)
+driver.find_element(By.XPATH,"//span[normalize-space()='Copy']").click()
+
+time.sleep(3)
+driver.switch_to.alert.accept()
