17841b12be1e8cf026f39293bd30f0102930e295
Shah Newaj <newajuiucse@gmail.com>
2022-08-10 09:53:33 +0600

    Bootstrap Dropdown

---
+++ day 23_Selenium/BootstrapDropdown.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+serv_obj = Service("C:\Drivers\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(service=serv_obj)
+
+driver.get("https://www.dummyticket.com/dummy-ticket-for-visa-application/")
+driver.maximize_window()
+
+driver.find_element(By.XPATH,"//span[@id='select2-billing_country-container']").click()
+
+countrylist = driver.find_elements(By.XPATH,"//ul[@id='select2-billing_country-results']/li")
+print(len(countrylist))
+
+for country in countrylist:
+    if country.text=="Bangladesh":
+        country.click()
+        break
