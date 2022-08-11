d4b599b93263fc837bdf2ae00339fe1008cb70a4
Shah Newaj <newajuiucse@gmail.com>
2022-08-11 13:32:25 +0600

    Excel Utility File

---
+++ day 24_Selenium/XLUtils.py
@@ -0,0 +1,46 @@
+import openpyxl
+from openpyxl.styles import PatternFill
+
+def getRowCount(file,sheetName):
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook[sheetName]
+    return(sheet.max_row)
+
+
+def getColumnCount(file,sheetName):
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook[sheetName]
+    return(sheet.max_column)
+
+
+def readData(file,sheetName,rownum,columnnum):
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook[sheetName]
+    return sheet.cell(rownum,columnnum).value
+
+
+def writeData(file,sheetName,rownum,columnnum,data):
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook[sheetName]
+    sheet.cell(rownum,columnnum).value = data
+    workbook.save(file)
+
+
+def fillGreenColor(file,sheetName,rownum,columnnum):
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook[sheetName]
+    greenFill = PatternFill(start_color='60b212',
+                            end_color='60b212',
+                            fill_type='solid')
+    sheet.cell(rownum,columnnum).fill = greenFill
+    workbook.save(file)
+
+
+def fillRedColor(file,sheetName,rownum,columnnum):
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook[sheetName]
+    redFill = PatternFill(start_color='fff0000',
+                          end_color='fff0000',
+                          fill_type='solid')
+    sheet.cell(rownum,columnnum).fill = redFill
+    workbook.save(file)
