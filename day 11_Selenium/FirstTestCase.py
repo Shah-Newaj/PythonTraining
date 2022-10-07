c748a84fba34626a3ca444cf17d48b03d4d52005
Shah-Newaj <newajuiucse@gmail.com>
2022-10-07 10:32:32 +0600

    First Test Case (EIMS V2)

--- day 11_Selenium/FirstTestCase.py
+++ day 11_Selenium/FirstTestCase.py
@@ -7,15 +7,15 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
-driver.get("https://opensource-demo.orangehrmlive.com/")
+driver.get("http://dceims.bdeducation.org.bd/schoolAdmin")
 driver.maximize_window()
 
-driver.find_element(By.NAME,"txtUsername").send_keys("Admin")
-driver.find_element(By.ID,"txtPassword").send_keys("admin123")
-driver.find_element(By.ID,"btnLogin").click()
+driver.find_element(By.NAME,"email").send_keys("marma.newaj@gmail.com")
+driver.find_element(By.ID,"password").send_keys("12345678")
+driver.find_element(By.XPATH,"//button[@type='submit']").click()
 
 act_title = driver.title
-exp_title = "OrangeHRM"
+exp_title = "EIMS Dashboard"
 
 if act_title == exp_title:
     print("Login test passed")
