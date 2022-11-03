cba92b705d8f779b39ef06fd81e621e48e7ca330
Shah-Newaj <newajuiucse@gmail.com>
2022-11-03 12:50:29 +0600

    Interview Prac

--- for Interview/prac_register.py
+++ for Interview/prac_register.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
@@ -28,3 +30,36 @@ driver.find_element(By.ID,"msdd").click()
 driver.find_element(By.XPATH,"//a[normalize-space()='English']").click()
 driver.find_element(By.XPATH,"//a[normalize-space()='Arabic']").click()
 driver.find_element(By.XPATH,"//a[normalize-space()='Urdu']").click()
+
+#Skills
+skill_element = driver.find_element(By.XPATH,"//select[@id='Skills']")
+skill = Select(skill_element)
+
+skill.select_by_visible_text("CSS")
+
+
+#Select Country
+country_element = driver.find_element(By.ID,"country")
+country = Select(country_element)
+country.select_by_value("India")
+
+#Date of Birth
+year_element = driver.find_element(By.ID,"yearbox")
+year = Select(year_element)
+year.select_by_visible_text("1991")
+
+month_element = driver.find_element(By.XPATH,"//select[@placeholder='Month']")
+month = Select(month_element)
+month.select_by_visible_text("April")
+
+date_element = driver.find_element(By.ID,"daybox")
+date = Select(date_element)
+date.select_by_visible_text("24")
+
+#Password
+driver.find_element(By.ID,"firstpassword").send_keys("12345")
+driver.find_element(By.ID,"secondpassword").send_keys("12345")
+
+#Refresh
+time.sleep(5)
+driver.find_element(By.ID,"Button1").click()
