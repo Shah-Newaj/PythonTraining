8ea8b4f22bce10edfb638308f83e48eb4888b7aa
Shah-Newaj <newajuiucse@gmail.com>
2022-11-04 11:02:05 +0600

    Mouse Hover

--- day 21_Selenium/MouseHover.py
+++ day 21_Selenium/MouseHover.py
@@ -4,16 +4,18 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 
-serv_obj = Service("C:\Drivers\chromedriver_win32\chromedriver.exe")
-driver = webdriver.Chrome(service=serv_obj)
+# serv_obj = Service("C:\Drivers\chromedriver_win32\chromedriver.exe")
+# driver = webdriver.Chrome(service=serv_obj)
+
+driver = webdriver.Chrome()
 
 driver.get("https://opensource-demo.orangehrmlive.com/")
 driver.maximize_window()
 
 #Login
-driver.find_element(By.NAME,"txtUsername").send_keys("Admin")
-driver.find_element(By.ID,"txtPassword").send_keys("admin123")
-driver.find_element(By.ID,"btnLogin").click()
+driver.find_element(By.XPATH,"//input[@placeholder='Username']").send_keys("Admin")
+driver.find_element(By.XPATH,"//input[@placeholder='Password']").send_keys("admin123")
+driver.find_element(By.XPATH,"//button[@type='submit']").click()
 time.sleep(3)
 
 #Admin->user management-> users
